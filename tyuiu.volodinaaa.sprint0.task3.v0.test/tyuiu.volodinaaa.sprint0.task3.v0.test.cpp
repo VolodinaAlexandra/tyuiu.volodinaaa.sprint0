58f38f618ad4dc96e9a1ac187f4d1489e4618631
tyuiu.volodinaaa@outlook.com <alexavolodina2006@gmail.com>
2023-09-08 16:16:53 +0300

    Task3

---
+++ tyuiu.volodinaaa.sprint0.task3.v0.test/tyuiu.volodinaaa.sprint0.task3.v0.test.cpp
@@ -0,0 +1,26 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include"../tyuiu.volodinaaa.sprint0.task3.v0.lib/tyuiu.volodinaaa.sprint0.task3.v0.lib.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest2
+{
+	TEST_CLASS(UnitTest2)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			ISprint0Task3* date = new Service1();
+			int a = 10;
+			int b = 9;
+			int c = 8;
+			int d;
+
+			d = date->SummV3(a,b,c);
+
+			Assert::AreEqual(27, d);
+		}
+	};
+}
